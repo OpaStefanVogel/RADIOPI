7329ae09b36e1425eaf52dac5ccf9c091e641931
Stefan_RPI4_Bookworm <backebackekuchen16@gmail.com>
2025-02-15 20:34:28 +0100

    proc Bild_aus

--- TCL-RADIO.tcl
+++ TCL-RADIO.tcl
@@ -11,12 +11,13 @@ pack [button .info -text "Info" -command "mache {Radio Info}"]
 pack [button .bus31 -text "Bus 31" -command "mache Bus31"]
 pack [button .bus62 -text "Bus 62" -command "mache Bus62"]
 pack [button .rpib -text "RPI B" -command RPIB]
+#pack [label .l1 -textvariable aktuelles_Gewicht]
 pack [button .ttyAMA5 -text "ttyAMA5" -command ttyAMA5]
 pack [button .wlan_ein -text "WLAN ein" -command "exec touch tmp/hostapd_start"]
 pack [button .wlan_aus -text "WLAN aus" -command "exec touch tmp/hostapd_stop"]
 pack [button .ansageein -text "Ansage an" -command "mache {Ansage an}"]
 pack [button .ansageaus -text "Ansage aus" -command "mache {Ansage aus}"]
-pack [button .bildaus -text "Bild aus" -command "destroy .c; toplevel .c -background black -cursor none; wm attributes .c -fullscreen 1; bind .c X { focus -force . ; destroy .c}; bind .c <Button-1> {destroy .c ; focus -force .}; bind .c E {.radioan invoke}; bind .c A {.radioaus invoke}; bind .c P {.radiolauter invoke}; bind .c M {.radioleiser invoke}; bind .c I {.info invoke}; bind .c B {.bus31 invoke}; bind .c C {.bus62 invoke}; "]
+pack [button .bildaus -text "Bild aus" -command "Bild_aus"]
 pack [button .swapoff -text "swapoff" -command "swapoffon"]
 pack [button .reboot -text "reboot" -command "sudoreboot"]
 
@@ -64,10 +65,40 @@ proc RPIB {} {
   }
 
 proc ttyAMA5 {} {
-  exec lxterminal -e "stty -F /dev/ttyAMA5 115200 raw cs8 -cstopb -parenb -crtscts -echo ixon -ixoff;stty raw -echo opost quit ^C isig ixon -ixoff; cp /dev/ttyAMA5 /dev/tty & cp /dev/tty /dev/ttyAMA5" &
-  exec echo >/dev/ttyS0 "55 66 77 M* M."
-  exec echo >/dev/ttyS0 "55 66 M* M. F0 EMIT 9D EMIT 84 EMIT 9E EMIT 20 EMIT"
+  exec lxterminal -e "stty -F /dev/ttyAMA5 115200 raw cs8 -cstopb -parenb -crtscts -echo ixon -ixoff" &
+  exec lxterminal -e "stty raw -echo opost quit ^C isig ixon -ixoff; cp /dev/ttyAMA5 /dev/tty & cp /dev/tty /dev/ttyAMA5" &
+#  exec echo >/dev/ttyS0 "55 66 77 M* M."
+#  exec echo >/dev/ttyS0 "55 66 M* M. F0 EMIT 9D EMIT 84 EMIT 9E EMIT 20 EMIT"
   }
+
+set aktuelles_Gewicht ""
+
+proc Bild_aus {} {
+  global aktuelles_Gewicht
+  destroy .c .c.l
+  toplevel .c -background black -cursor none
+  label .c.l -font {Arial 240} -textvariable aktuelles_Gewicht
+  pack .c.l
+  wm attributes .c -fullscreen 1; 
+  bind .c X { focus -force . ; destroy .c}; 
+  bind .c <Button-1> {destroy .c ; focus -force .}; 
+  bind .c E {.radioan invoke}; 
+  bind .c A {.radioaus invoke}; 
+  bind .c P {.radiolauter invoke}; 
+  bind .c M {.radioleiser invoke}; 
+  bind .c I {.info invoke}; 
+  bind .c B {.bus31 invoke}; 
+  bind .c C {.bus62 invoke}; 
+  }
+
+#exec mkfifo Tst.txt
+#puts [pwd]
+#set f [open "Tst.txt" r+ ]
+#puts $f
+#chan configure $f -blocking 0 -buffering none -translation binary
+#chan event $f readable [puts "Hiho"]
+
+#after 10000 { set aktuelles_Gewicht "hehehheheheheee"; puts "Hiha"; chan puts $f "hiiiiihaaaaa"; chan flush $f }
 
 proc swapoffon {} {
   exec sudo swapoff /dev/zram0 
@@ -87,8 +118,6 @@ proc sudoreboot {} {
   }
 
 
-
-
 proc serverOpen {channel addr port} {
     global connected
     set connected 1
@@ -99,15 +128,18 @@ proc serverOpen {channel addr port} {
 
 proc readLine {who channel} {
     global didRead
+  global aktuelles_Gewicht
     if { [gets $channel line] < 0} {
 	fileevent $channel readable {}
 	after idle "close $channel;set out 1"
     } else {
 	puts "READ LINE: $line"
-	puts $channel "Return of $line"
-	flush $channel;
+#	puts $channel "Return of $line"
+#	flush $channel
+	eval $line
+	puts "eval $aktuelles_Gewicht"
 	set didRead 1
-	focus -force .
+#	focus -force .
     }
 }
 
@@ -115,7 +147,5 @@ set connected 0
 # catch {socket -server serverOpen 9900} server
 if {$Test == 0} {set server [socket -server serverOpen 9900]}
 
-
-
-#vwait forever
+#vwait forever; #nur wenn kein TK
 
