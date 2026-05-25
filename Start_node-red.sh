b8a4c35626b067d48b56ee687570a2c7a102cb09
Stefan_RPI4_dietpi_Trixie <backebackekuchen16@gmail.com>
2026-05-25 18:18:17 +0200

    git add Start_node-red.sh

---
+++ Start_node-red.sh
@@ -0,0 +1,4 @@
+cd /home/dietpi/Desktop/CLONE/RADIOPI/
+alias node-red='/mnt/dietpi_userdata/node-red/node_modules/.bin/node-red'
+/mnt/dietpi_userdata/node-red/node_modules/.bin/node-red -u node-red flows_Radio.json &
+cd
